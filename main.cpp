ae8994647366cccc0d88e64bdc461fb992e998a5
reemakhandelwal <94159340+reemakhandelwal@users.noreply.github.com>
2022-11-24 20:08:30 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-// const int V=5;
-// const int V=4;
+const int V=5;
 // const int V=4;
 // const int V=3;
-const int V=3;
+//const int V=3;
 
 bool checkCycle(int cVertex, int pVertex, int arr[][V], vector<int>& order, int visited[V]) {
     visited[cVertex] = 1;
     order.push_back(cVertex);
     for(int i=0; i<V; ++i) {
+        //if adjacent vertex and not visited
         if(arr[cVertex][i] && !visited[i]) {
             if(checkCycle(i, cVertex, arr, order, visited)) {
                 return true;
@@ -18,6 +18,7 @@ bool checkCycle(int cVertex, int pVertex, int arr[][V], vector<int>& order, int 
         }
     }
     for(int i=0; i<V; ++i) {
+        //if adjacent vertex and visited and is not a parent vertex and not a current vertex
         if(arr[cVertex][i] && visited[i] && i!=pVertex && i!=cVertex) {
             order.push_back(i);
             return true;
@@ -38,11 +39,10 @@ bool cycle(int arr[][V], vector<int>& order) {
 }
 
 int main() {
-    // int arr[][V] = {{0, 1, 0, 0, 0}, {1, 0, 1, 0, 1}, {0, 1, 0, 1, 0}, {0, 0, 1, 0, 1}, {0, 1, 0, 1, 0}};
+    int arr[][V] = {{0, 1, 0, 0, 0}, {1, 0, 1, 0, 1}, {0, 1, 0, 1, 0}, {0, 0, 1, 0, 1}, {0, 1, 0, 1, 0}};
     // int arr[][V] = {{0, 1, 0, 0}, {1, 0, 1, 1}, {0, 1, 0, 1}, {0, 1, 1, 0}};
-     int arr[][V] = {{0, 1, 0, 1}, {1, 0, 1, 1}, {0, 1, 0, 1}, {1, 1, 1, 0}};
     // int arr[][V] = {{0, 1, 1}, {1, 0, 1}, {1, 1, 0}};
-   // int arr[][V] = {{0, 1, 0}, {1, 0, 1}, {0, 1, 0}};
+    // int arr[][V] = {{0, 1, 0}, {1, 0, 1}, {0, 1, 0}};
 
     vector<int> order(V+1);
     order.assign(order.size(), -1);
